90dfecffc960279143683cb87c85d23e6a660222
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2019-07-25 16:05:43 +0100

    Correction to derived exceptions

--- megaminx/exceptions.h
+++ megaminx/exceptions.h
@@ -6,7 +6,7 @@
 #define DERIVED_EXCEPTION(TYPE) \
 class TYPE : public std::runtime_error { \
     public: \
-      TYPE(const std::string m) : std::runtime_error(m) {} \
+      TYPE(const std::string& m) : std::runtime_error(m) {} \
       TYPE(const char* m) : std::runtime_error(m) {} \
 };
 
@@ -16,5 +16,10 @@ namespace Megaminx {
    * Exception thrown if the megaminx faces are not connected as expected
    */
   DERIVED_EXCEPTION(connection_error);
+
+  /**
+   * Exception thrown if megaminx face fails to parse from string given
+   */
+  DERIVED_EXCEPTION(parse_error);
   
 }
