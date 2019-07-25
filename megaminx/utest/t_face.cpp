9f22c36ec0bf8fe5189548f9908288ec876f5ff6
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2019-07-25 15:31:45 +0100

    add connected_edge_facets and test

--- megaminx/utest/t_face.cpp
+++ megaminx/utest/t_face.cpp
@@ -5,9 +5,9 @@
 
 #define EXPECT_ARRAY_EQ(array1,array2) \
 {\
-  EXPECT_EQ(array1.size(),array2.size());\
-  for (int i=0;i<array1.size();++i) {\
-    EXPECT_EQ(array1[i],array2[i]);\
+  EXPECT_EQ((array1).size(),(array2).size());\
+  for (int i=0;i<(array1).size();++i) {\
+    EXPECT_EQ((array1)[i],(array2)[i]);\
   }\
 }
 
@@ -74,19 +74,19 @@ TEST(FaceTest,edge_facets)
   EXPECT_EQ(f.str(),"abcdefghij");
   auto e = f.edge_facets(0);
   std::array<char,3> expected = {'a','b','c'};
-  EXPECT_ARRAY_EQ((*e),expected);
+  EXPECT_ARRAY_EQ(*e,expected);
   e = f.edge_facets(1);
   expected = {'c','d','e'};
-  EXPECT_ARRAY_EQ((*e),expected);
+  EXPECT_ARRAY_EQ(*e,expected);
   e = f.edge_facets(2);
   expected = {'e','f','g'};
-  EXPECT_ARRAY_EQ((*e),expected);
+  EXPECT_ARRAY_EQ(*e,expected);
   e = f.edge_facets(3);
   expected = {'g','h','i'};
-  EXPECT_ARRAY_EQ((*e),expected);
+  EXPECT_ARRAY_EQ(*e,expected);
   e = f.edge_facets(4);
   expected = {'i','j','a'};
-  EXPECT_ARRAY_EQ((*e),expected);
+  EXPECT_ARRAY_EQ(*e,expected);
 }
 
 TEST(FaceTest,set_edge_facets)
@@ -128,6 +128,28 @@ TEST(FaceTest,connecting_edge)
   EXPECT_EQ(f2->connecting_edge('x'),2);
 }
 
+TEST(FaceTest,connected_edge_facets)
+{
+  auto f1 = std::make_shared<Megaminx::Face>('x');
+  char c = 'a';
+  for(int i=0;i<10;++i) {
+    f1->set_facet(i,c+i);
+  }
+  EXPECT_THROW(f1->connected_edge_facets(0),Megaminx::connection_error);
+  auto f2 = std::make_shared<Megaminx::Face>('x');
+  c = 'A';
+  for(int i=0;i<10;++i) {
+    f2->set_facet(i,c+i);
+  }
+  f1->connect(0,f2);
+  EXPECT_THROW(f1->connected_edge_facets(0),Megaminx::connection_error);
+  f2->connect(1,f1);
+  std::array<char,3> expected = {'C','D','E'};
+  EXPECT_ARRAY_EQ(*f1->connected_edge_facets(0),expected);
+  expected = {'a','b','c'};
+  EXPECT_ARRAY_EQ(*f2->connected_edge_facets(1),expected);
+}
+
 
 //----- Death Tests
 TEST(FaceDeathTest,facets)
@@ -161,6 +183,13 @@ TEST(FaceDeathTest,set_edge_facets)
   std::array<char,3> facets = {'1','2','3'};
   EXPECT_DEATH(f.set_edge_facets(5,facets), "Assertion failed");
   EXPECT_DEATH(f.set_edge_facets(-1,facets), "Assertion failed");
+}
+
+TEST(FaceDeathTest,connected_edge_facets)
+{
+  Megaminx::Face f('w');
+  EXPECT_DEATH(f.connected_edge_facets(5), "Assertion failed");
+  EXPECT_DEATH(f.connected_edge_facets(-1), "Assertion failed");
 }
 
 
