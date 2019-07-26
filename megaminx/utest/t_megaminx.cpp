160cf87a6fe9b686e8077ea8b74d73c6b4f6ddd3
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2019-07-26 15:32:47 +0100

    Improve megaminx constructor test

--- megaminx/utest/t_megaminx.cpp
+++ megaminx/utest/t_megaminx.cpp
@@ -8,4 +8,6 @@ TEST(MegaminxTest,Constructor)
 {
   Megaminx::Megaminx m;
   EXPECT_EQ(m.face(0)->colour(), 'w');
+  EXPECT_EQ(m.face(0)->opposite_face()->colour(), 'x');
+  EXPECT_EQ(m.face(0)->connected_face(0)->colour(), 'r');
 }
