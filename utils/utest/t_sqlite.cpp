ee1c41d1e9da93a93653b81dea0667297a49da02
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2019-07-28 20:39:44 +0100

    Further fixes for sqlite

--- utils/utest/t_sqlite.cpp
+++ utils/utest/t_sqlite.cpp
@@ -1,11 +1,11 @@
 #include <gtest/gtest.h>
 
-#include <sqlite3.h>
+#include <sqlite3/sqlite3.h>
 
 TEST(SQLiteTest,Basic)
 {
   sqlite3* db;
-  int status = sqlite3_open('./sqlite.db', &db);
+  int status = sqlite3_open("./sqlite.db", &db);
   sqlite3_close(db);
 
 }
